625903374f6aa2cc001c11199714856d01c19404
nishit singh123 <ns3579singh@gmail.com>
2025-03-19 20:30:08 +0530

    add codes

--- chapter7.py
+++ chapter7.py
@@ -1,20 +1,20 @@
 #function & recurrsion
 # function definition
-# def avg ():
-#     a =int(input("enter your number:"))
-#     b=int(input("enter your number: "))
-#     c=int(input("enter your number:"))
+def avg ():
+    a =int(input("enter your number:"))
+    b=int(input("enter your number: "))
+    c=int(input("enter your number:"))
 
-#     average =(a+b+c)/3
-#     print(average)
-# avg()#fuction call
-# print("thank you!")
-# avg()
-# print("thank you!")
-# avg()
-# print("thank you!")
-# avg()
-# avg()
+    average =(a+b+c)/3
+    print(average)
+avg()#fuction call
+print("thank you!")
+avg()
+print("thank you!")
+avg()
+print("thank you!")
+avg()
+avg()
 def goodDay():
     print("good day")
 goodDay()    
